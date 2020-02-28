9a7c43dd96fc204a497dfb3c53517cc46c22a6ca
Pauline Vos <pvos88@gmail.com>
2020-02-28 14:03:50 +0100

    Override `userInfo` response
    So that it has a test user with the structure we're expecting.

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const { OAuth2Server } = require('oauth2-mock-server')
 
-let server = new OAuth2Server()
-
 async function startServer() {
+  const server = new OAuth2Server()
   await server.issuer.keys.generateRSA();
   await server.start(8080, 'localhost');
+
+  return server.service
 }
 
 startServer()
-  .then(() => console.log('Issuer URL:', server.issuer.url))
+  .then((service) => {
+    service.on('beforeUserinfo', (userInfoResponse, req) => {
+      userInfoResponse.body = {
+        "email": "someone-social@facebook.com",
+        "firstName": "Party",
+        "lastName": "Pete",
+      }
+    })
+  })
   .catch((e) => console.log('Something went wrong starting the server: ', e))
